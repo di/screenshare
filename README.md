82247e19cd71f6506b7845df1570f7306bca7ad3
Adam Faur <adam.faur@gmail.com>
2017-07-04 23:15:00 -0700

    Update to python3 Update readme

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ Super-simple screen sharing with nothing but Python and Flask.
 
     pip install pyscreenshot
     pip install flask
+    pip install pillow
+
+# Install
+
+    ./scripts/install
 
 # Running
 
-    python screen.py
+    ./scripts/run
 
 Then visit [http://localhost:5000](http://localhost:5000).
 
--- src/screen.py
+++ src/screen.py
@@ -1,16 +1,21 @@
 import pyscreenshot
 import flask
-from StringIO import StringIO
+from io import BytesIO
 
 app = flask.Flask(__name__)
 
 
 @app.route('/screen.png')
 def serve_pil_image():
-    img_buffer = StringIO()
+    img_buffer = BytesIO()
     pyscreenshot.grab().save(img_buffer, 'PNG', quality=50)
     img_buffer.seek(0)
     return flask.send_file(img_buffer, mimetype='image/png')
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return flask.send_from_directory('js', path)
 
 
 @app.route('/')
@@ -18,4 +23,4 @@ def serve_img():
     return flask.render_template('screen.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
